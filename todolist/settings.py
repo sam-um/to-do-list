64968371a175436995ee1fa31aac308e183e87be
umyoung <wooyoung_@hotmail.com>
2022-12-26 19:29:43 -0800

    Add Allowed Host

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = ['myawesomedjangotestapp.herokuapp.com']
+ALLOWED_HOSTS = ['myawesomedjangotestapp.herokuapp.com', 'to-do-list-umyoung.herokuapp.com']
 
 
 # Application definition

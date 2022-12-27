2bab4d0828b453335fcfc886e8d16ea6844025e7
umyoung <samuel.wooyoung.um@gmail.com>
2022-12-26 16:57:30 -0800

    Initial Commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
 ALLOWED_HOSTS = ['myawesomedjangotestapp.herokuapp.com']
 
